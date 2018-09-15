614207ad460b71bf7fdf3c2707cbf4f295881bdf
sharmithaT <tsharmitha@gmail.com>
2018-09-14 22:18:08 -0400

    sukeerthi's admin commit

--- src/main/java/com/onlinequiz/mvc/controller/RegisterController.java
+++ src/main/java/com/onlinequiz/mvc/controller/RegisterController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+
 public class RegisterController {
     @Autowired
     RestTemplate restTemplate;
@@ -20,6 +22,7 @@ public class RegisterController {
     public ModelAndView registrationPage(HttpServletRequest request) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("registeration");
+
 
 
         ResponseEntity<Register[]> responseEntity = restTemplate.getForEntity("http://localhost:8070/register", Register[].class);
